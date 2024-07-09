d4a0ae22aaf2334fab9ec33c0ff03b96b9831232
shubham43MP <shubhamdixit19950321@gmail.com>
2024-07-09 18:02:05 +0530

    test commit 2

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,10 +4,10 @@ module.exports = {
   
     // Lint & Prettify TS and JS files
     "**/*.(ts|tsx|js)": filenames => [
-      `npm run eslint ${filenames.join(" ")}`,
-      `npm run prettier --write ${filenames.join(" ")}`,
+      `yarn eslint ${filenames.join(" ")}`,
+      `yarn prettier --write ${filenames.join(" ")}`,
     ],
   
     // Prettify only Markdown and JSON files
-    "**/*.(md|json)": filenames => `npm run prettier --write ${filenames.join(" ")}`,
+    "**/*.(md|json)": filenames => `yarn prettier --write ${filenames.join(" ")}`,
   };
